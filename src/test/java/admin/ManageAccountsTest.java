7616cf3f6c4fda0fa88c8534469322b614d8a401
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-13 14:34:36 +0100

    Tests de gestion de compte (côté admin)

---
+++ src/test/java/admin/ManageAccountsTest.java
@@ -0,0 +1,38 @@
+package admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ManageAccountsTest {
+
+	@Test
+	public void createAccountTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void deleteAccountTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void modifyAccountPasswordTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void addAccountWarningTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void createProfileTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void blockAccountTest() {
+		fail("Not yet implemented");
+	}
+}
