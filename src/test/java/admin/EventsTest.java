6810ac6452ed59d5bf9fed9a5b26acd833ce5353
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-13 14:35:05 +0100

    Tests de gestion des événements (côté admin)

---
+++ src/test/java/admin/EventsTest.java
@@ -0,0 +1,33 @@
+package admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventsTest {
+
+	@Test
+	public void getEventsListTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void createEventTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void createCommonEventTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void validateEventTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void deleteEventTest() {
+		fail("Not yet implemented");
+	}
+}
