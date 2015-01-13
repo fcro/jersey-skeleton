925908e80d176154e0a4759f25915fbdad3c503f
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-13 14:38:24 +0100

    Test du panel d'administration

---
+++ src/test/java/admin/AdminPanelTest.java
@@ -0,0 +1,18 @@
+package admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminPanelTest {
+
+	@Test
+	public void adminConnectTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void adminDisconnectTest() {
+		fail("Not yet implemented");
+	}
+}
