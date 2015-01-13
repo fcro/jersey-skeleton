edd527facdccec435bffb9e5be50f466442c6a79
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-13 16:36:44 +0100

    Suppression de YoungAgain.java + servlet de login

--- src/main/java/fr/youngagain/Login.java
+++ src/main/java/fr/youngagain/Login.java
@@ -4,19 +4,23 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class Login {
+@WebServlet("/Login")
+public class Login extends HttpServlet {
 	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+		String login = req.getParameter("login");
+		String password = req.getParameter("paswd");
+
+		if (connect(login, password) == true)
+			res.sendRedirect("pageAccueil.html");
+		else
+			res.sendRedirect("index.html?bl=1");
 	}
 
-	public User connect(String login, String password) {
-		if (login.equals("toto") && password.equals("titi")) {
-			
-		}
-		return null;
+	private boolean connect(String login, String password) {
+		return (login.equals("toto") && password.equals("titi"));
 	}
 }
