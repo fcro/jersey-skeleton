38bf9da104fca84c6d2a1e57d224c0967ee5969c
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-13 15:48:15 +0100

    Écriture des tests de ManageAccountsTest

--- src/test/java/fr/youngagain/test/admin/ManageAccountsTest.java
+++ src/test/java/fr/youngagain/test/admin/ManageAccountsTest.java
@@ -3,39 +3,62 @@ package fr.youngagain.test.admin;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import fr.youngagain.admin.ManageAccount;
 
 public class ManageAccountsTest {
 
+	@BeforeClass
+	public static void init() {
+		ManageAccount.createAccount("Dug", "dug", "dug", "pword");
+		ManageAccount.createAccount("Fu", "fu", "fu", "dfgjd");
+		ManageAccount.blockAccount("fu");
+	}
+
 	@Test
 	public void createAccountTest() {
-		assertTrue(createAccount("Dupont", "Gabozumeu", "dupontg", "password"));
+		assertTrue(ManageAccount.createAccount("Dupont", "Gabozumeu", "dupontg", "password"));
+	}
+
+	@Test(expected=LoginAlreadyUsedException.class)
+	public void LoginAlreadyUsedExceptionTest() {
+		ManageAccount.createAccount("Dug", "dug", "dug", "pword");
 	}
 
 	@Test
 	public void deleteAccountTest() {
-		assertFalse(deleteAccount("totod"));
-		createAccount("Dumond", "toto", "totod", "pswd");
-		assertTrue(deleteAccount("totod"));
-		assertFalse(deleteAccount("totod"));
+		assertFalse(ManageAccount.deleteAccount("totod"));
+		ManageAccount.createAccount("Dumond", "toto", "totod", "pswd");
+		assertTrue(ManageAccount.deleteAccount("totod"));
+		assertFalse(ManageAccount.deleteAccount("totod"));
 	}
 
 	@Test
 	public void modifyAccountPasswordTest() {
-		createAccount("Chose", "Truc", "mach", "ps");
-		assertTrue(modifyPassword("mach", "peuceu", "peuceu", "ps"));
+		ManageAccount.createAccount("Chose", "Truc", "mach", "ps");
+		assertTrue(ManageAccount.modifyPassword("mach", "peuceu", "peuceu", "ps"));
+	}
+
+	@Test(expected=AccountNotFoundException.class)
+	public void accountNotFoundExceptionTest() {
+		ManageAccount.modifyPassword("cecomptenexistepas", "abc", "abc", "def");
+		ManageAccount.deleteAccount("cecomptenexistepas");
 	}
 
 	@Test(expected=PasswordFormatException.class)
-	public void modifyAccountPasswordTest() {
-		
+	public void modifyAccountPasswordExceptionTest() {
+		ManageAccount.createAccount("bidule", "agneugneu", "brlbrl", "secret");
+		ManageAccount.modifyPassword("brlbrl", "secret", "secret", "secret");
+		ManageAccount.modifyPassword("brlbrl", "secre", "secret", "secret");
+		ManageAccount.modifyPassword("brlbrl", "secret", "secret", "sret");
 	}
 
 	@Test
 	public void addAccountWarningTest() {
-		fail("Not yet implemented");
+		assertTrue(ManageAccount.sendWarning("dug"));
+		assertFalse(ManageAccount.sendWarning("gud"));
 	}
 
 	@Test
@@ -45,6 +68,11 @@ public class ManageAccountsTest {
 
 	@Test
 	public void blockAccountTest() {
-		fail("Not yet implemented");
+		assertTrue(ManageAccount.blockAccount("dug"));
+	}
+
+	@Test(expected=AccountBlockedException.class)
+	public void accountBlockedExceptionTest() {
+		ManageAccount.blockAccount("fu");
 	}
 }
