d546dd04ba1f239933f81ec37cf2f5ff1684ecb4
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-14 10:29:00 +0100

    Création de la classe DBConnector

---
+++ src/main/java/fr/youngagain/database/DBConnector.java
@@ -0,0 +1,30 @@
+package fr.youngagain.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	private static Connection c = null;
+
+	public static Connection getConnection() throws SQLException {
+		try {
+			if (c == null | c.isClosed()) {
+				Class.forName("org.h2.Driver");
+				c = DriverManager.getConnection("jdbc:h2:youngagain");
+			}
+		} catch (ClassNotFoundException cnfe) {
+			throw new SQLException("Échec de connexion à la base de données.");
+		} catch (SQLException sqle) {
+			throw new SQLException("Échec de connexion à la base de données.");
+		}
+
+		return c;
+	}
+
+	public static void closeConnection() {
+		try {
+			c.close();
+		} catch (SQLException e) {}
+	}
+}
