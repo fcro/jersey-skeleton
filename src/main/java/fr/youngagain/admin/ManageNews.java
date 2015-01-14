9a7ede9f1a03b106e78395e732a3b3f16ee12578
damien <dam.toulouse@gmail.com>
2015-01-14 11:53:08 +0100

    ajout du @webservlet .......

--- src/main/java/fr/youngagain/admin/ManageNews.java
+++ src/main/java/fr/youngagain/admin/ManageNews.java
@@ -3,10 +3,11 @@ package fr.youngagain.admin;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet ("/ManageNews")
 public class ManageNews extends HttpServlet {
 	private static NewsPaper news=new NewsPaper();
 	public void service(HttpServletRequest req, HttpServletResponse res)
