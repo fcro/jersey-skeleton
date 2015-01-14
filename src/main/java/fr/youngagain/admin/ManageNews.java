3d7b7c418677a9b8de37c8a684acb34136656d4e
damien <dam.toulouse@gmail.com>
2015-01-14 11:43:49 +0100

    form dans newsAdmin.html + servlet ManageNews

---
+++ src/main/java/fr/youngagain/admin/ManageNews.java
@@ -0,0 +1,26 @@
+package fr.youngagain.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ManageNews extends HttpServlet {
+	private static NewsPaper news=new NewsPaper();
+	public void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		String s = req.getParameter("act");
+		if(s.equals("add")){
+			news.addNews(new News( ));
+		}
+		if(s.equals("del")){
+			news.remove(new News());
+		}
+		if(s.equals("up")){
+			news.update(new News());
+		}
+		
+	}
+}
