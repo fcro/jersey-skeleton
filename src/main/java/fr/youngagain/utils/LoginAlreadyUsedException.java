107915351de37bcf10404b5c4e7b3f35ccebafad
damien <dam.toulouse@gmail.com>
2015-01-15 09:42:23 +0100

    Constructeur des exceptions

--- src/main/java/fr/youngagain/utils/LoginAlreadyUsedException.java
+++ src/main/java/fr/youngagain/utils/LoginAlreadyUsedException.java
@@ -1,5 +1,11 @@
 package fr.youngagain.utils;
 
 public class LoginAlreadyUsedException extends Exception {
-
+	private String message;
+	public LoginAlreadyUsedException() {
+		// TODO Auto-generated constructor stub
+	}
+	public LoginAlreadyUsedException(String message) {
+		this.message=message;
+	}
 }
--- src/main/java/fr/youngagain/utils/PasswordFormatException.java
+++ src/main/java/fr/youngagain/utils/PasswordFormatException.java
@@ -1,5 +1,11 @@
 package fr.youngagain.utils;
 
 public class PasswordFormatException extends Exception {
-
+	private String message;
+	public PasswordFormatException() {
+		// TODO Auto-generated constructor stub
+	}
+	public PasswordFormatException(String message) {
+		this.message=message;
+	}
 }
