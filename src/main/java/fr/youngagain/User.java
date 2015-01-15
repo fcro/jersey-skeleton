36b72b9eb5189333c0cf5ab782ab44e82062aa51
Camille BREUZON <camillebreuzon@orange.fr>
2015-01-15 17:36:18 +0100

    matchage entre personne

---
+++ src/main/java/fr/youngagain/User.java
@@ -0,0 +1,43 @@
+package fr.youngagain;
+
+public class User {
+	private String login, mdp, nom, prenom, ville, role, photo;
+	private char sexe;
+	private boolean fumeur;
+	private Criteres c;
+	
+	
+	public User(String login, String mdp, char sexe, boolean fumeur, Criteres c){
+		this.login = login;
+		this.mdp = mdp;
+		this.sexe = sexe;
+		this.fumeur = fumeur;
+		this.c = c;
+	}
+	
+	public String getLogin(){
+		return this.login;				
+	}
+	
+	public String getMdp(){
+		return this.mdp;
+	}
+	
+	public boolean isFumeur(){
+		return fumeur;
+	}
+	
+	public boolean isMale(){
+		return (sexe=='h');
+	}
+	
+	public boolean isFemale(){
+		return (sexe=='f');
+	}
+	
+	public Criteres getCriteres(){
+		return c;
+	}
+	
+	
+}
