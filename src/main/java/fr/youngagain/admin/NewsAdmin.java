f8114b85165ccce44ca6b65eb7f5ec0394fb138c
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-01-15 10:03:40 +0100

    Déplacement newsAdmin.html + création servlet AccountAdmin + passage newsAdmin en servlet

--- src/main/java/fr/youngagain/admin/NewsAdmin.java
+++ src/main/java/fr/youngagain/admin/NewsAdmin.java
@@ -1,6 +1,8 @@
 package fr.youngagain.admin;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.youngagain.PHProcks;
+
 @WebServlet("/newsAdmin")
 public class NewsAdmin extends HttpServlet {
 
 	public void service(HttpServletRequest req, HttpServletResponse res)
 		throws ServletException, IOException {
-		
+			res.setContentType("text/html");
+			PrintWriter out = res.getWriter();
+			PHProcks.include(out, new File("resources/includes/newsAdmin.html"));
 	}
-	
+
 }
